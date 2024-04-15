ceae280430af0d6a3d4e498d261a7523df9aefc3
NeilTohno <neil_tohno@live.com>
2024-04-15 18:08:05 +0800

    Add files via upload

---
+++ mpv_Launcher.bat
@@ -0,0 +1,9 @@
+CD/D "%~dp0"
+:: 需要自己改地址
+set PATH="C:\ZHD_PG\mpv\portable_config"
+
+C:\ZHD_PG\mpv\mpv.com --force-window=yes --idle --config-dir="C:\ZHD_PG\mpv\portable_config"
+
+::C:\ZHD_PG\mpv\mpv.com "%~1"
+
+echo. & pause
